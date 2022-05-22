e90b6c08b1d99d8f6f752a3864f81c5b0781f2e9
ejasquith <emily.asquith@icloud.com>
2022-05-22 18:42:50 +0000

    Add initial code to script.js and create piece.js

---
+++ assets/js/piece.js
@@ -0,0 +1,13 @@
+class Piece {
+    constructor() {
+
+    }
+
+    getValidMoves() {
+
+    }
+
+    move() {
+        
+    }
+}
---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    
+    initialiseBoard();
+});
+
+function initialiseBoard() {
+    
+}

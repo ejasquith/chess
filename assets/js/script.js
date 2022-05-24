7bdc3536a5d8e38f3735effb15c3d935761dc0bc
ejasquith <emily.asquith@icloud.com>
2022-05-24 10:29:46 +0000

    Implement Bishop.getValidMoves

--- assets/js/script.js
+++ assets/js/script.js
@@ -125,7 +125,68 @@ class Knight extends Piece {
 }
 
 class Bishop extends Piece {
+    getValidMoves() {
+        let moves = [];
+        let rank = this.position.rank;
+        let file = this.position.file;
+        while (rank < 7 && file < 7) {
+            rank++;
+            file++;
+            if (board[rank][file] !== undefined) {
+                if (board[rank][file].colour !== this.colour) {
+                    moves.push([rank, file]);
+                }
+                break;
+            } else {
+                moves.push([rank, file]);
+            }
+        }
 
+        rank = this.position.rank;
+        file = this.position.file;
+        while (rank < 7 && file > 0) {
+            rank++;
+            file--;
+            if (board[rank][file] !== undefined) {
+                if (board[rank][file].colour !== this.colour) {
+                    moves.push([rank, file]);
+                }
+                break;
+            } else {
+                moves.push([rank, file]);
+            }
+        }
+
+        rank = this.position.rank;
+        file = this.position.file;
+        while (rank > 0 && file < 7) {
+            rank--;
+            file++;
+            if (board[rank][file] !== undefined) {
+                if (board[rank][file].colour !== this.colour) {
+                    moves.push([rank, file]);
+                }
+                break;
+            } else {
+                moves.push([rank, file]);
+            }
+        }
+
+        rank = this.position.rank;
+        file = this.position.file;
+        while (rank > 0 && file > 0) {
+            rank--;
+            file--;
+            if (board[rank][file] !== undefined) {
+                if (board[rank][file].colour !== this.colour) {
+                    moves.push([rank, file]);
+                }
+                break;
+            } else {
+                moves.push([rank, file]);
+            }
+        }
+    }
 }
 
 class King extends Piece {

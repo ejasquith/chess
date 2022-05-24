aecf70bcbc9e93b2d14c18c95279336c994ea2d0
ejasquith <emily.asquith@icloud.com>
2022-05-24 09:08:32 +0000

    Fix logic for Rook.getValidMoves

--- assets/js/script.js
+++ assets/js/script.js
@@ -50,47 +50,47 @@ class Pawn extends Piece {
 class Rook extends Piece {
     getValidMoves() {
         let moves = [];
-        let rank = this.position.rank;
-        let file = this.position.file;
-        for (rank; rank <= 7; rank++) {
-            if (board[rank][file] !== undefined) {
-                if (board[rank][file].colour !== this.colour) {
-                    moves.push([rank][file]);
+        // let rank = this.position.rank;
+        // let file = this.position.file;
+        for (let rank = this.position.rank + 1; rank <= 7; rank++) {
+            if (board[rank][this.position.file] !== undefined) {
+                if (board[rank][this.position.file].colour !== this.colour) {
+                    moves.push([rank][this.position.file]);
                 }
                 break;
             } else {
-                moves.push([rank][file]);
+                moves.push([rank][this.position.file]);
             }
         }
-        for (rank; rank >= 0; rank--) {
-            if (board[rank][file] !== undefined) {
-                if (board[rank][file].colour !== this.colour) {
-                    moves.push([rank][file]);
+        for (let rank = this.position.rank - 1; rank >= 0; rank--) {
+            if (board[rank][this.position.file] !== undefined) {
+                if (board[rank][this.position.file].colour !== this.colour) {
+                    moves.push([rank][this.position.file]);
                 }
                 break;
             } else {
-                moves.push([rank][file]);
+                moves.push([rank][this.position.file]);
             }
         }
 
-        for (file; file <= 7; file++) {
-            if (board[rank][file] !== undefined) {
-                if (board[rank][file].colour !== this.colour) {
-                    moves.push([rank][file]);
+        for (let file = this.position.file + 1; file <= 7; file++) {
+            if (board[this.position.rank][file] !== undefined) {
+                if (board[this.position.rank][file].colour !== this.colour) {
+                    moves.push([this.position.rank][file]);
                 }
                 break;
             } else {
-                moves.push([rank][file]);
+                moves.push([this.position.rank][file]);
             }
         }
-        for (file; file >= 0; file--) {
-            if (board[rank][file] !== undefined) {
-                if (board[rank][file].colour !== this.colour) {
-                    moves.push([rank][file]);
+        for (let file = this.position.file - 1; file >= 0; file--) {
+            if (board[this.position.rank][file] !== undefined) {
+                if (board[this.position.rank][file].colour !== this.colour) {
+                    moves.push([this.position.rank][file]);
                 }
                 break;
             } else {
-                moves.push([rank][file]);
+                moves.push([this.position.rank][file]);
             }
         }
 

bb0119c9b76724613d31362d2c155a55b7c10609
ejasquith <emily.asquith@icloud.com>
2022-05-28 10:11:06 +0000

    Add methods and properties to game class

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,13 +1,28 @@
 export default class Game {
     static instance = undefined;
     constructor() {
-
+        this.activePlayer = 'white';
+        this.turn = 1;
+        this.history = [];
     }
 
-    getInstance() {
+    static getInstance() {
         if (Game.instance === undefined) {
             Game.instance = new Game();
         }
         return Game.instance;
+    }
+
+    updateHistory(piece, oldCoords, newCoords) {
+        this.history.push([piece, oldCoords, newCoords]);
+    }
+
+    updateTurn() {
+        if (this.activePlayer === 'black') {
+            this.turn++;
+            this.activePlayer = 'white';
+        } else {
+            this.activePlayer = 'black';
+        }
     }
 }

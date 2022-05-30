80fd75fcb4af8a7507d38f7b58cc2c1ff9dcbcf8
ejasquith <emily.asquith@icloud.com>
2022-05-30 08:52:07 +0000

    Add check logic to remaining moves in Queen.getValidMoves

--- assets/js/queen.js
+++ assets/js/queen.js
@@ -7,42 +7,106 @@ export default class Queen extends Piece {
         for (let rank = this.position.rank + 1; rank <= 7; rank++) {
             if (board[rank][this.position.file] !== undefined) {
                 if (board[rank][this.position.file].colour !== this.colour) {
-                    moves.push([rank, this.position.file]);
+                    if (!lookForChecks ||
+                        !Board.getInstance().findChecks(this.colour, {
+                        oldCoords: [this.position.rank, this.position.file],
+                        newCoords: [rank, this.position.file],
+                        ep: false,
+                        castle: false
+                    })) {
+                        moves.push([rank, this.position.file]);
+                    }
                 }
                 break;
             } else {
-                moves.push([rank, this.position.file]);
+                if (!lookForChecks ||
+                    !Board.getInstance().findChecks(this.colour, {
+                    oldCoords: [this.position.rank, this.position.file],
+                    newCoords: [rank, this.position.file],
+                    ep: false,
+                    castle: false
+                })) {
+                    moves.push([rank, this.position.file]);
+                }
             }
         }
         for (let rank = this.position.rank - 1; rank >= 0; rank--) {
             if (board[rank][this.position.file] !== undefined) {
                 if (board[rank][this.position.file].colour !== this.colour) {
-                    moves.push([rank, this.position.file]);
+                    if (!lookForChecks ||
+                        !Board.getInstance().findChecks(this.colour, {
+                        oldCoords: [this.position.rank, this.position.file],
+                        newCoords: [rank, this.position.file],
+                        ep: false,
+                        castle: false
+                    })) {
+                        moves.push([rank, this.position.file]);
+                    }
                 }
                 break;
             } else {
-                moves.push([rank, this.position.file]);
+                if (!lookForChecks ||
+                    !Board.getInstance().findChecks(this.colour, {
+                    oldCoords: [this.position.rank, this.position.file],
+                    newCoords: [rank, this.position.file],
+                    ep: false,
+                    castle: false
+                })) {
+                    moves.push([rank, this.position.file]);
+                }
             }
         }
 
         for (let file = this.position.file + 1; file <= 7; file++) {
             if (board[this.position.rank][file] !== undefined) {
                 if (board[this.position.rank][file].colour !== this.colour) {
-                    moves.push([this.position.rank, file]);
+                    if (!lookForChecks ||
+                        !Board.getInstance().findChecks(this.colour, {
+                        oldCoords: [this.position.rank, this.position.file],
+                        newCoords: [this.position.rank, file],
+                        ep: false,
+                        castle: false
+                    })) {
+                        moves.push([this.position.rank, file]);
+                    }
                 }
                 break;
             } else {
-                moves.push([this.position.rank, file]);
+                if (!lookForChecks ||
+                    !Board.getInstance().findChecks(this.colour, {
+                    oldCoords: [this.position.rank, this.position.file],
+                    newCoords: [this.position.rank, file],
+                    ep: false,
+                    castle: false
+                })) {
+                    moves.push([this.position.rank, file]);
+                }
             }
         }
         for (let file = this.position.file - 1; file >= 0; file--) {
             if (board[this.position.rank][file] !== undefined) {
                 if (board[this.position.rank][file].colour !== this.colour) {
-                    moves.push([this.position.rank, file]);
+                    if (!lookForChecks ||
+                        !Board.getInstance().findChecks(this.colour, {
+                        oldCoords: [this.position.rank, this.position.file],
+                        newCoords: [this.position.rank, file],
+                        ep: false,
+                        castle: false
+                    })) {
+                        moves.push([this.position.rank, file]);
+                    }
                 }
                 break;
             } else {
-                moves.push([this.position.rank, file]);
+                if (!lookForChecks ||
+                    !Board.getInstance().findChecks(this.colour, {
+                    oldCoords: [this.position.rank, this.position.file],
+                    newCoords: [this.position.rank, file],
+                    ep: false,
+                    castle: false
+                })) {
+                    moves.push([this.position.rank, file]);
+                }
             }
         }
 
